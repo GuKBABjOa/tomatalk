9a3a4a69ee8b34d05b856b979579ba6836ab6e5a
강주빈 <sisoya0424@gmail.com>
2025-01-21 13:49:07 +0900

    init: 183-be-프로젝트-초기-세팅

---
+++ backend/tolonbgeub/src/main/java/team/overfullow/tolonbgeub/TolonbgeubApplication.java
@@ -0,0 +1,13 @@
+package team.overfullow.tolonbgeub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TolonbgeubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TolonbgeubApplication.class, args);
+	}
+
+}
