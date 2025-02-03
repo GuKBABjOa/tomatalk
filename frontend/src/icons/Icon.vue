442a756c4aacf1c8a54e2107c59a0d8ef0100c61
진성일 <wls9804@naver.com>
2025-02-03 13:44:59 +0900

    feature-#226-아이콘

---
+++ frontend/src/icons/Icon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+    name: string;
+    size?: string;
+}>();
+
+
+const iconPath = computed(() => new URL(`../assets/${props.name}.svg`, import.meta.url).href);
+</script>
+
+<template>
+    <img :src="iconPath" :alt="name" :width="size || '24px'" :height="size || '24px'" />
+</template>
