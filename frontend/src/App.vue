2ddab32aaea84907c0a809895b534ae23fd8c569
sungjoon <happypigs7.dev@gmail.com>
2025-02-08 23:40:54 +0900

    init: 프론트 초기 세팅

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,11 @@
-<script setup>
-import { RouterView } from 'vue-router';
-</script>
-
 <template>
-  <div id="app-container" class="h-screen">
-    <component :is="$route.meta.layout || 'div'" class="h-full">
-      <router-view v-slot="{ Component }">
-        <component :is="Component" />
-      </router-view>
-    </component>
+  <div id="app" class="min-h-screen flex items-center justify-center bg-gray-100">
+    <h1 class="text-3xl font-bold text-gray-900">DeepBate 프로젝트 🚀</h1>
   </div>
 </template>
 
-<style>
-#app {
-  display: flex;
-  justify-content: center;
-}
+<script setup lang="ts">
+</script>
 
-#app-container {
-  background-color: rgb(255, 255, 255);
-}
-
-@media (min-width: 390px) {
-  #app-container {
-    width: 600px;
-  }
-}
+<style scoped>
 </style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,4 @@
 import './assets/main.css'
-import './registerServiceWorker'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
