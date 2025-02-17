ca1fbfdcb83e715fb86ea53b04bbae0d4d204f68
방성준 <happypigs7@gmail.com>
2025-02-17 13:21:43 +0900

    feat: #462 AI 토론 참여자

--- backend/fastapi/app/audio/audio_router.py
+++ backend/fastapi/app/audio/audio_router.py
@@ -20,4 +20,19 @@ async def websocket_endpoint(websocket: WebSocket, db: AsyncSession = Depends(ge
     """
     await audio_transcription(websocket, db)
 
+# ✅ WebSocket STT API 엔드포인트
+@router.get("/ai_bot")
+async def websocket_endpoint(websocket: WebSocket, db: AsyncSession = Depends(get_async_db)):
+    """
+    웹 소켓 연결을 통해 오디오 데이터를 실시간으로 텍스트로 변환하는 API 엔드포인트
+
+    Args:
+        websocket (WebSocket): FastAPI WebSocket 인스턴스
+        db (AsyncSession): 데이터베이스 연결 객체
+
+    Returns:
+        웹 소켓을 통해 변환된 텍스트 데이터를 전송
+    """
+    await audio_transcription(websocket, db)
+
 
